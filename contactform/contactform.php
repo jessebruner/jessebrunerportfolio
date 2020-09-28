ef7950477c4938bc875250f706e493e69d4f71eb
Jesse Bruner <jebruner1690@gmail.com>
2020-09-28 16:08:32 -0400

    update

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,10 +4,10 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Email: $email \n Subject: $subject \n Message: $message";
-$recipient = "jessebruner@outlook.com";
-$subject = "$name contacted you through jessebruner.com";
-$mailheader = "From: jessebruner@outlook.com \r\n";
+$recipient = "jebruner1690@gmail.com";
+$subject = "$name sent you a message";
+$mailheader = "From: jebruner@wccnet.edu \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-header("Location:index.html");
+header("Location:../index.html");
 //echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#4c4c4c;'> Return Home</a>";
 ?>
